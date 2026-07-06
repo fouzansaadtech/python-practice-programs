71256fd966f85959f98680e46439fe35c69b1ffd
Fouzan Saad H <fouzansaad.tech@gmail.com>
2026-07-06 15:01:08 +0530

    12_tuple_occurrence_counter

---
+++ 12_tuple_occurrence_counter.py
@@ -0,0 +1,7 @@
+#Count the Occurrence of an Element
+
+# WAP to count how many times the number 10 appears in the tuple
+
+numbers = (10, 20, 10, 30, 10, 40) 
+count=numbers.count(10)
+print(f"10 appears {count} times.")
