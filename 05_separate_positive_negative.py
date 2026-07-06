cd12136620572c747101e1230ffad733f92e7652
Fouzan Saad H <fouzansaad.tech@gmail.com>
2026-07-06 14:57:53 +0530

    05_separate_positive_negative

---
+++ 05_separate_positive_negative.py
@@ -0,0 +1,11 @@
+#Separate Positive and Negative Numbers 
+ 
+numbers = [10, -5, 20, -15, 30, -8, -9] 
+print("Negative:")
+for i in numbers:
+     if i <=0:
+      print(i)
+print("Postive:")
+for i in numbers:
+   if i>=0:
+      print(i)
